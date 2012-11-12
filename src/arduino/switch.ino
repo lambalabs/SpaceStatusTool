5aedf2987393150877590f4d461893a3832fe160
Marc Farra <marcfarra@gmail.com>
2012-11-12 20:23:04 +0200

    Update comments

--- src/arduino/switch.ino
+++ src/arduino/switch.ino
@@ -11,40 +11,38 @@ Twitter twitter("938006472-wQp8DRWV5bbUfz5yxd5N7gK2vr63UKdbgC8pK7WB");
 char msg_on[] = "Someone is working in the space!";
 char msg_off[] = "No one is currently in the space.";
 
+// PostMsg takes a message and an update number
+// Update number required to avoid Twitter duplicate message error.
 void postMsg(char* msg, int num) {
   Serial.println("connecting ...");
   String toPost = String("Update #") + num + " " + String(msg);
   char buf[140];
   toPost.toCharArray(buf,140);
   if (twitter.post(buf)) {
-    // Specify &Serial to output received response to Serial.
-    // If no output is required, you can just omit the argument, e.g.
-    // int status = twitter.wait();
     int status = twitter.wait(&Serial);
     if (status == 200) {
-      Serial.println("OK.");
+      if (D) Serial.println("OK.");
     } else {
-      Serial.print("failed : code ");
-      Serial.println(status);
+      if (D) Serial.print("failed : code ");
+      if (D) Serial.println(status);
     }
   } else {
-    Serial.println("connection failed.");
+    if (D) Serial.println("connection failed.");
   }
 }
 
 int current_state = 0;
 int last_state = 0;
 int sw = 7;
+int D = 0;
 
 void setup()
 {
   delay(1000);
   pinMode(sw, INPUT);
-  digitalWrite(sw, HIGH);
+  digitalWrite(sw, HIGH); // Use internal pull-up resistor
   Ethernet.begin(mac);
-  // or you can use DHCP for autoomatic IP address configuration.
-  // Ethernet.begin(mac);
-  Serial.begin(9600);
+  if (D) Serial.begin(9600);
 
 }
 int update_number = 1;
@@ -55,8 +53,8 @@ void loop()
   current_state = digitalRead(sw);
   if (current_state != last_state)
   {
-    Serial.println("posting new message!");
-    Serial.println(current_state);
+    if (D) Serial.println("posting new message!");
+    if (D) Serial.println(current_state);
     if (current_state == 1)
     {
       postMsg(msg_on, update_number);
